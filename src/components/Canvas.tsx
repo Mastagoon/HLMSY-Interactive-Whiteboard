9eae24e56b37844a193e48d40c15a88bc24bda67
Mastagoon <inzilagiway@gmail.com>
2022-07-24 14:14:39 +0300

    Env

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -31,7 +31,7 @@ const Canvas: React.FC<CanvasProps> = ({ roomId }) => {
   const socketInitializer = async () => {
     if (connected) return
     // await fetch("/api/socketio")
-    socket = io(process.env.SERVER_HOST ?? "http://localhost:8080")
+    socket = io(process.env.NEXT_PUBLIC_SERVER_HOST ?? "http://localhost:8080")
 
     socket.on("connect", () => {
       console.log("connected")
