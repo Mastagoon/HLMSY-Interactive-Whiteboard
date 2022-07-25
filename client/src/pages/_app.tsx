fff74b573806de848260755d7212a75d1e8f143b
Mastagoon <inzilagiway@gmail.com>
2022-07-25 16:18:31 +0300

    ?

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/assets/icons/favicon/favicon.ico" />
         <title>Here, let me show you</title>
       </Head>
       <CanvasProvider>
